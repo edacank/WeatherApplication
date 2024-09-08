ec60250933902bfa165db00c13d1e27926027e37
edacank <edacan1924@gmail.com>
2024-09-08 13:54:28 +0300

    summary
    ASPNET Core MVC Weather Application

---
+++ WeatherApplication/Controllers/WeatherController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+public class WeatherController : Controller
+{
+    private WeatherService _weatherService;
+
+    public WeatherController()
+    {
+        _weatherService = new WeatherService();
+    }
+
+    public async Task<ActionResult> Index(string city)
+    {
+        if (string.IsNullOrEmpty(city))
+        {
+            city = "Bursa"; 
+        }
+
+        var weather = await _weatherService.GetWeatherAsync(city);
+        ViewBag.Weather = weather;
+        ViewBag.City = city;
+
+        return View();
+    }
+}
---
+++ WeatherApplication/WeatherService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+public class WeatherService
+{
+    private static readonly HttpClient client = new HttpClient();
+    private string apiKey = ""; //my API key
+
+    public async Task<string> GetWeatherAsync(string city)
+    {
+        string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+        var response = await client.GetStringAsync(url);
+
+        // JSON response decomposition
+        var weatherData = JObject.Parse(response);
+        var temperature = weatherData["main"]["temp"].ToString();
+        var description = weatherData["weather"][0]["description"].ToString();
+
+        return $"Temperature: {temperature}°C, Description: {description}";
+    }
+}
